f0a642e39f2af3485cacba58936c0e23195a14d7
津嶋勇輝 <67349483+tsushima3074@users.noreply.github.com>
2022-09-30 11:18:39 +0900

    jsonを返すプログラム

--- study/web_app/main.go
+++ study/web_app/main.go
@@ -1,10 +1,17 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"log"
 	"net/http"
 )
+
+type response struct {
+	Name string `json:"name"`
+	Age  int    `json:"page"`
+}
 
 func main() {
 	router := gin.Default()
@@ -18,14 +25,14 @@ func main() {
 	})
 
 	// フォームの内容を受け取って挨拶する --- (*3)
-	router.GET("/hello", func(ctx *gin.Context) {
+	router.GET("/get", func(ctx *gin.Context) {
 		name := ctx.Query("name")
 		ctx.Header("Content-Type", "text/html; charset=UTF-8")
 		ctx.String(200, "<h1>Hello, "+name+"</h1>")
 	})
 
-	router.POST("/db", func(ctx *gin.Context) {
-		name := ctx.DefaultPostForm("name1", "何もない")
+	router.POST("/post", func(ctx *gin.Context) {
+		name := ctx.DefaultPostForm("name", "何もない")
 		ctx.Header("Content-Type", "text/html; charset=UTF-8")
 		ctx.String(200, "<h1>dbに以下を登録するよ: "+name+"</h1>")
 	})
@@ -38,12 +45,28 @@ func main() {
 		ctx.String(200, "<h1>"+message+"</h1>")
 	})
 
-	router.GET("/get_json", func(ctx *gin.Context) {
-		name := ctx.Param("name")
-		action := ctx.Param("action")
-		message := name + " is " + action
-		ctx.Header("Content-Type", "text/html; charset=UTF-8")
-		ctx.String(200, "<h1>"+message+"</h1>")
+	//router.POST("/get_json", func(ctx *gin.Context) {
+	//	name := ctx.DefaultPostForm("name", "何もない")
+	//	age, _ := strconv.Atoi(ctx.DefaultPostForm("age", "0"))
+	//	food := ctx.DefaultPostForm("food", "何もない")
+	//	sampleJson := &response{
+	//		name: name,
+	//		age:  age,
+	//		food: food,
+	//	}
+	//	ctx.JSON(200, sampleJson)
+	//})
+
+	router.GET("/get_json_test", func(ctx *gin.Context) {
+		sampleJson := &response{
+			Name: "name",
+			Age:  21}
+		fmt.Println(sampleJson)
+		res3B, _ := json.Marshal(sampleJson)
+		fmt.Println(res3B)
+		fmt.Println(string(res3B))
+		//ctx.Header("Content-Type", "application/json; charset=utf-8")
+		ctx.JSON(200, string(res3B))
 	})
 
 	// サーバーを起動
